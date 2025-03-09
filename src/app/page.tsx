2bf1ff127697d95843c6e97d1269d359728f964b
Kenspectacle <Kennyftw2@gmail.com>
2025-03-09 22:19:31 +0100

    feat: stretched the page out until the bottom for aesthetics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)] bg-black">
-      <h1 className="text-white">getDocs, fine tuning your beloved AI in a few clicks!</h1>
+    <div className="font-[family-name:var(--font-geist-sans)] h-full bg-black ">
+      <h1 className="text-white">flex-auto getDocs, fine tuning your beloved AI in a few clicks!</h1>
     </div>
   );
 }
