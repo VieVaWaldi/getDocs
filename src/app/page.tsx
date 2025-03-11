08d7e8d06b7d1afca92a0b37f8c364cf74f633d1
Kenspectacle <Kennyftw2@gmail.com>
2025-03-11 23:17:29 +0100

    feat: added a button from shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)] h-full bg-black flex justify-center items-center">
       <h1 className="text-white">getDocs, fine tuning your beloved AI in a few clicks!</h1>
+      <Button>Get Started</Button>
     </div>
   );
 }
