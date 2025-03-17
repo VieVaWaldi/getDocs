0da2645fea9df3122d1e765b78c2bd13758aa33a
Kenspectacle <Kennyftw2@gmail.com>
2025-03-17 20:49:05 +0100

    fix: removed hard-coded dark mode homepage style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)] h-screen bg-black flex flex-col justify-center items-center">
-      <h1 className="text-white">getDocs, fine tuning your beloved AI in a few clicks!</h1>
+    <div className="font-[family-name:var(--font-geist-sans)] h-screen flex flex-col justify-center items-center">
+      <h1>getDocs, fine tuning your beloved AI in a few clicks!</h1>
       <Button>Get Started</Button>
     </div>
   );
