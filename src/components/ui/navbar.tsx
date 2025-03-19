98783333629cd2c7db6309b5bf44052fa07e787b
Kenspectacle <Kennyftw2@gmail.com>
2025-03-19 21:06:07 +0100

    feat: added basic functionality to the navbar

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,3 +1,5 @@
+
+
 import {
     NavigationMenu,
     NavigationMenuContent,
@@ -11,15 +13,29 @@ import {
 
 export default function Navbar() {
     return (
-    <NavigationMenu>
-        <NavigationMenuList>
-            <NavigationMenuItem>
-                <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                    <NavigationMenuLink>Link</NavigationMenuLink>
-                </NavigationMenuContent>
-            </NavigationMenuItem>
-        </NavigationMenuList>
-    </NavigationMenu>
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      Home
+                    </div>
+                  </a>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/login"
+                  >
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      Login
+                    </div>
+                  </a>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
     )
 }
